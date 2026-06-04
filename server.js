59c8f09266f4989a7603d037ed9d5151991c4708
Nivetha3101 <nivi.m31032004@gmail.com>
2026-06-04 10:13:43 +0530

    Fix deployed app routing and use dynamic PORT for hosting

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ const server = http.createServer(async (req, res) => {
   res.end();
 });
 
-server.listen(3000, () => {
-  console.log('Bulk Mailer running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Bulk Mailer running on port ${PORT}`);
 });
